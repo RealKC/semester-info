6f68866f2e4d0cd7cfa335e30b9395a3ce2ffc81
Mițca Dumitru <dumitru0mitca@gmail.com>
2025-11-19 00:00:51 +0200

    Move `SemesterWidget`'s main content `Composable` to a new file
    Should make it easier to add unit tests for this

--- app/src/main/java/dev/realkc/semesterinfo/widget/SemesterWidget.kt
+++ app/src/main/java/dev/realkc/semesterinfo/widget/SemesterWidget.kt
@@ -1,28 +1,15 @@
 package dev.realkc.semesterinfo.widget
 
 import android.content.Context
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
-import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
-import androidx.glance.layout.Alignment
-import androidx.glance.layout.Column
-import androidx.glance.layout.fillMaxSize
-import androidx.glance.layout.padding
-import androidx.glance.text.FontFamily
-import androidx.glance.text.FontWeight
-import androidx.glance.text.Text
-import androidx.glance.text.TextStyle
-import androidx.glance.unit.ColorProvider
 import dev.realkc.semesterinfo.R
-import dev.realkc.semesterinfo.WeekInfo
 import dev.realkc.semesterinfo.weekInfo
 import dev.realkc.semesterinfo.widget.composables.TextColorProvider
+import dev.realkc.semesterinfo.widget.composables.WeekInfoDisplay
 
 class SemesterWidget : GlanceAppWidget() {
     override suspend fun provideGlance(
@@ -34,7 +21,7 @@ class SemesterWidget : GlanceAppWidget() {
         provideContent {
             GlanceTheme {
                 TextColorProvider { color ->
-                    MyContent(info, textColor = color)
+                    WeekInfoDisplay(info, textColor = color)
                 }
             }
         }
@@ -48,32 +35,8 @@ class SemesterWidget : GlanceAppWidget() {
 
         provideContent {
             GlanceTheme {
-                MyContent(info, textColor = Color.Black)
+                WeekInfoDisplay(info, textColor = Color.Black)
             }
-        }
-    }
-
-    @Composable
-    private fun MyContent(
-        week: WeekInfo,
-        textColor: Color,
-    ) {
-        Column(
-            modifier = GlanceModifier.fillMaxSize(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                text = week.description,
-                style =
-                    TextStyle(
-                        fontFamily = FontFamily.Monospace,
-                        fontSize = 32.sp,
-                        color = ColorProvider(textColor),
-                        fontWeight = FontWeight.Bold,
-                    ),
-                modifier = GlanceModifier.padding(12.dp),
-            )
         }
     }
 }
---
+++ app/src/main/java/dev/realkc/semesterinfo/widget/composables/WeekInfoDisplay.kt
@@ -0,0 +1,43 @@
+package dev.realkc.semesterinfo.widget.composables
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.glance.GlanceModifier
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.text.FontFamily
+import androidx.glance.text.FontWeight
+import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
+import dev.realkc.semesterinfo.WeekInfo
+
+@Composable
+fun WeekInfoDisplay(
+    week: WeekInfo,
+    textColor: Color,
+    modifier: GlanceModifier = GlanceModifier,
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = week.description,
+            style =
+                TextStyle(
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 32.sp,
+                    color = ColorProvider(textColor),
+                    fontWeight = FontWeight.Bold,
+                ),
+            modifier = GlanceModifier.padding(12.dp),
+        )
+    }
+}
